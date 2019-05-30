3781da591c4196c580a7ce129d6881b7e0563adb
Rudson Carvalho <rudson.carvalho@gmail.com>
2019-05-30 00:59:45 -0300

    adicionando repositorio remoto

--- cvc-rf-domain/src/main/java/br/com/cvc/rf/domain/Transferencia.java
+++ cvc-rf-domain/src/main/java/br/com/cvc/rf/domain/Transferencia.java
@@ -2,17 +2,13 @@ package br.com.cvc.rf.domain;
 
 public class Transferencia {
 
-	public Transferencia() {
-		this.codigo = 1;
-	}
-	
-	private int codigo;
+	private String uuid;
 
-	public int getCodigo() {
-		return codigo;
+	public String getUuid() {
+		return uuid;
 	}
 
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
 	}
 }
--- cvc-rf-services/src/main/java/br/com/cvc/rf/services/controller/TransferenciaController.java
+++ cvc-rf-services/src/main/java/br/com/cvc/rf/services/controller/TransferenciaController.java
@@ -1,6 +1,5 @@
 package br.com.cvc.rf.services.controller;
 
-import org.springframework.hateoas.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
