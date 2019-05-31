6360ab7b89afe29947143a7e3b0e478cc877957f
Rudson Carvalho <rudson.carvalho@gmail.com>
2019-05-30 23:16:24 -0300

    mapper domain to dto vice versa

--- cvc-rf-api/src/main/java/br/com/cvc/rf/api/dto/AccountDTO.java
+++ cvc-rf-api/src/main/java/br/com/cvc/rf/api/dto/AccountDTO.java
@@ -5,7 +5,31 @@ import br.com.cvc.rf.domain.Account;
 public class AccountDTO {
 
 	public AccountDTO(Account account) {
-		// TODO Auto-generated constructor stub
+	
+		this.name = account.getName();
+		this.email = account.getEmail();
 	}
+	
+	private String name;
+	
+	private String email;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 
 }
--- cvc-rf-domain/src/main/java/br/com/cvc/rf/domain/Account.java
+++ cvc-rf-domain/src/main/java/br/com/cvc/rf/domain/Account.java
@@ -4,9 +4,12 @@ public class Account {
 
 	public Account() {
 		this.name = "Ted";
+		this.email = "ted@email.com";
 	}
 	
 	private String name;
+	
+	private String email;
 
 	public String getName() {
 		return name;
@@ -14,6 +17,14 @@ public class Account {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	
 	
